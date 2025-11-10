a096fd318eaa473ce8bd787240846673ebddfa95
ManishKr16 <mkraja1314@gmail.com>
2025-11-10 12:26:05 +0530

    Add files via upload

---
+++ lab_7/task4.py
@@ -0,0 +1,8 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+r1 = Rectangle(10, 5)
+print("Length:", r1.length)
+print("Width:", r1.width)
